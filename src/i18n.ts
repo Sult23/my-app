f3867d06324b835ea7b4ed7a8dda6375a51081fb
Sultan <sultan.nurmatov>
2024-02-19 16:24:11 +0300

    fixes

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,7 +1,6 @@
 import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
 import Backend from 'i18next-http-backend'
-import LanguageDetector from 'i18next-browser-languagedetector'
 import translationEN from './locales/en/translation.json'
 import translationRU from './locales/ru/translation.json'
 import translationSR from './locales/sr/translation.json'
@@ -20,7 +19,6 @@ const resources = {
 
 i18n
   .use(Backend)
-  .use(LanguageDetector)
   .use(initReactI18next)
   .init({
     resources,
